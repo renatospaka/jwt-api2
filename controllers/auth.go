11d89c985535d57847fa62d13da714e9b091378a
Renato Spakauskas <renatospaka@gmail.com>
2021-07-24 23:02:12 -0300

    feat: :sparkles: login with an user
    now we have a cookie to handle user connections

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,15 +1,14 @@
 package controller
 
 import (
-	"log"
 	"strconv"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 	"github.com/renatospaka/go-jwt/database"
 	models "github.com/renatospaka/go-jwt/models/user"
-	"golang.org/x/crypto/bcrypt"
 )
 
 const SecretKey = "secret"
@@ -39,6 +38,7 @@ func Register(c *fiber.Ctx) error {
 	DB.Create(&user)
 	return c.Status(fiber.StatusOK).JSON(user)
 }
+
 
 func Login(c *fiber.Ctx) error {
 	var data map[string]string
@@ -83,54 +83,16 @@ func Login(c *fiber.Ctx) error {
 			"message": err.Error(),
 		})
 	}
-	log.Printf("%v", token)
 
-	// var data map[string]string
-
-	// if err := c.BodyParser(&data); err != nil {
-	// 	c.Status(fiber.StatusBadRequest)
-	// 	return c.JSON(fiber.Map{
-	// 		"message": err.Error(),
-	// 	})
-	// }
-
-	// user := models.User{}
-	// DB := database.Connect()
-	// DB.Where("email = ?", data["email"]).First((&user))
-	// if user.ID == 0 {
-	// 	c.Status(fiber.StatusNotFound)
-	// 	return c.JSON(fiber.Map{
-	// 		//"message": "user not found",
-	// 		"message": "incorrect user or password",
-	// 	})
-	// }
-
-	// if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
-	// 	c.Status(fiber.StatusBadRequest)
-	// 	return c.JSON(fiber.Map{
-	// 		"message": "incorrect user or password",
-	// 	})
-	// }
-
-	// //issuer := strconv.Itoa(int(user.ID))
-	// issuer := strconv.FormatUint(uint64(user.ID), 10)
-	// claim := &jwt.StandardClaims{
-	// 	Issuer:    issuer,
-	// 	ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
-	// }
-	// claims := jwt.NewWithClaims(jwt.SigningMethodES256, claim)
-
-	// mySecret := []byte(SecretKey)
-	// token, err := claims.SignedString(mySecret)
-	// if err != nil {
-	// 	c.Status(fiber.StatusInternalServerError)
-	// 	return c.JSON(fiber.Map{
-	// 		//"message": "login failed",
-	// 		"message": err.Error(),
-	// 	})
-	// }
-
-	// //return c.Status(fiber.StatusOK).JSON(token)
-	// return c.JSON(token)
-	return c.Status(fiber.StatusOK).JSON(token)
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "success",
+	})
 }
--- main.go
+++ main.go
@@ -3,34 +3,18 @@ package main
 import (
 	"log"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
-	//"github.com/renatospaka/go-jwt/database"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/renatospaka/go-jwt/routes"
 )
 
 func main() {
 	//database.Connect()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 	route.Setup(app)
-	//createToken()
 
 	log.Fatal(app.Listen(":8000"))
-}
-
-func createToken() {
-	log.Println("creating token")
-	mySigningKey := []byte("AllYourBase")
-
-	// Create the Claims
-	claims := &jwt.StandardClaims{
-		ExpiresAt: 15000,
-		Issuer:    "test",
-	}
-	log.Printf("ExpiresAt: %T %v\n", claims.ExpiresAt, claims.ExpiresAt)
-	log.Printf("Issuer: %T %v\n", claims.Issuer, claims.Issuer)
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString(mySigningKey)
-	log.Printf("%v %v", ss, err)
 }
